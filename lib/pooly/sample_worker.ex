3a6222cb2552b96bf470c598a3a39670ecb210d6
Ilyes Ahidar <i.ahidar@smartphonehoesjes.nl>
2017-02-21 16:18:55 +0100

    Version 2 of Pooly

---
+++ lib/pooly/sample_worker.ex
@@ -0,0 +1,11 @@
+defmodule Pooly.SampleWorker do
+  use GenServer;
+
+  def start_link(_), do: GenServer.start_link(__MODULE__, :ok, [])
+
+  def stop(pid), do: GenServer.call(pid, :stop)
+
+  def init(_), do: {:ok, %{}}
+
+  def handle_call(:stop, _from, state), do: {:stop, :normal, :ok, state}
+end
